f5989632a8b76c40a3bf9c8f0ece8786e2356c2c
ArtemInyushev <art10inyushev@gmail.com>
2021-09-29 00:35:43 +0300

    lab2 task4

--- labs/lab1/app/src/main/java/com/example/lab1/MainActivity.java
+++ labs/lab1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,23 +3,33 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    private Spinner OperationSpin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        OperationSpin = (Spinner) view
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.operations, R.layout.spinner_item);
-        adapter.setDropDownViewResource(R.layout.spinner_item);
-        OperationSpin.setAdapter(adapter);
+        TextView Header = (TextView) findViewById(R.id.Header);
+
+        TableLayout table = (TableLayout) findViewById(R.id.Table);
+        int rowsCount = table.getChildCount();
+        for(int i = 0; i < rowsCount; i++){
+            TableRow row = (TableRow) table.getChildAt(i);
+            int spinnersCount = row.getChildCount();
+            for(int j  = 0; j < spinnersCount; j++){
+                Spinner spinner = (Spinner) row.getChildAt(j);
+                ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.operations, R.layout.spinner_item);
+                adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+                spinner.setAdapter(adapter);
+                Header.setText(Integer.toString(spinner.getWidth()));
+            }
+        }
     }
 }
