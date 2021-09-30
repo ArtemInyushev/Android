6df9223756b2edf4ee42c741d3c95db31b3e2e28
ArtemInyushev <art10inyushev@gmail.com>
2021-09-30 23:04:13 +0300

    lab2 task6 complete

--- labs/lab1/app/src/main/java/com/example/lab1/MainActivity.java
+++ labs/lab1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean changed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ChangeColor(View view){
+        Button button  = (Button) view;
+        if(changed){
+            button.setBackgroundResource(R.drawable.my_button);
+            changed = false;
+        }
+        else{
+            button.setBackgroundResource(R.drawable.my_button_clicked);
+            changed = true;
+        }
     }
 }
