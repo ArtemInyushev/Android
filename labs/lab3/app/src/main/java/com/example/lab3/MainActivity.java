901887abba3645dbd644ffe9fb951ae451a4df36
ArtemInyushev <art10inyushev@gmail.com>
2021-10-10 21:59:01 +0300

    lab3 task1 complete

--- labs/lab3/app/src/main/java/com/example/lab3/MainActivity.java
+++ labs/lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button USABtn;
+    private Button EnglandBtn;
+    private Button ItalyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        USABtn = (Button) findViewById(R.id.USABtn);
+        EnglandBtn = (Button) findViewById(R.id.EnglandBtn);
+        ItalyBtn = (Button) findViewById(R.id.ItalyBtn);
+        SetButtonHandlers();
+    }
+
+    private void SetButtonHandlers() {
+        USABtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent();
+                in.setClass(MainActivity.this, UsaActivity.class);
+                startActivity(in);
+            }
+        });
+        EnglandBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent();
+                in.setClass(MainActivity.this, EnglandActivity.class);
+                startActivity(in);
+            }
+        });
+        ItalyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent();
+                in.setClass(MainActivity.this, ItalyActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
