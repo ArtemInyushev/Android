d32f79d07265bdd6c1fd7745fae03d3dbb03b84b
ArtemInyushev <art10inyushev@gmail.com>
2021-10-16 00:41:41 +0300

    lab3 task5 complete

--- labs/lab3/app/src/main/java/com/example/lab3/MainActivity.java
+++ labs/lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -3,17 +3,86 @@ package com.example.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText Url;
+    private Button SearchBtn;
+    private Button PrevBtn;
+    private Button NextBtn;
+    private WebView Browser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView browser = (WebView) findViewById(R.id.webBrowser);
-        browser.loadUrl("https://drive.google.com/file/d/1l5l0F0wCJaYs1XiqebrprJWsdMiVuznW/view");
+        Url = (EditText) findViewById(R.id.Url);
+        SearchBtn = (Button) findViewById(R.id.SearchBtn);
+        PrevBtn = (Button) findViewById(R.id.PrevBtn);
+        NextBtn = (Button) findViewById(R.id.NextBtn);
+
+        Browser = (WebView) findViewById(R.id.webBrowser);
+        WebSettings webSettings = Browser.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        Browser.loadUrl(getString(R.string.start_url));
+
+        Browser.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Update();
+            }
+        });
+
+        SetButtonHandlers();
+    }
+
+    private void SetButtonHandlers(){
+        SearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Browser.loadUrl(Url.getText().toString());
+            }
+        });
+        PrevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Browser.canGoBack()){
+                    Browser.goBack();
+                }
+            }
+        });
+        NextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Browser.canGoForward()){
+                    Browser.goForward();
+                }
+            }
+        });
+    }
+
+    private void Update(){
+        Url.setText(Browser.getUrl());
+        if(Browser.canGoForward()){
+            NextBtn.setEnabled(true);
+        }
+        else {
+            NextBtn.setEnabled(false);
+        }
+        if(Browser.canGoBack()){
+            PrevBtn.setEnabled(true);
+        }
+        else {
+            PrevBtn.setEnabled(false);
+        }
     }
 }
