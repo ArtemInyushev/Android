93cb76d4b2b1fe8a422eb4666da35a8aca24b59a
ArtemInyushev <art10inyushev@gmail.com>
2021-12-06 00:25:11 +0200

    lab5 task3 complete

--- labs/lab5/app/src/main/java/com/example/lab5/MyView.java
+++ labs/lab5/app/src/main/java/com/example/lab5/MyView.java
@@ -4,73 +4,81 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.View;
+
+class Point{
+    public float X;
+    public float Y;
+
+    public Point(float x, float y){
+        X = x;
+        Y = y;
+    }
+}
+
 public class MyView extends View {
-    int x = 0;
-    int y = 0;
-    int widthRect = 300;
-    int heightRect = 200;
-    int strokeWidth = 20;
-    int vx = 10;
-    int vy = 10;
-    int blue = 0;
-    int green = 0;
-    int red = 0;
-    int madX = 0;
-    int madY = 0;
+    private int iteration = 0;
 
     public MyView(Context context) {
         super(context);
     }
 
-    void modifyRect() {
-        strokeWidth = (int)(Math.random()*100);
-        blue = (int)(Math.random()*255);
-        green = (int)(Math.random()*255);
-        red = (int)(Math.random()*255);
-        madX = (int)(Math.random()*100 - 50);
-        madY = (int)(Math.random()*100 - 50);
+    private void SierpinskiFunc(Point[] points, Canvas canvas, int depth){
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        boolean finish = false;
+        if(depth >= iteration) {
+            paint.setStyle(Paint.Style.FILL);
+            finish = true;
+        }
+        else{
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(5);
+            depth++;
+        }
+        Path path = new Path();
+        path.moveTo(points[0].X, points[0].Y);
+        path.lineTo(points[1].X, points[1].Y);
+        path.lineTo(points[2].X, points[2].Y);
+        path.lineTo(points[0].X, points[0].Y);
+        path.close();
+
+        canvas.drawPath(path, paint);
+
+        if(finish) return;
+        Point middleLeft = new Point((points[0].X + points[1].X) / 2,
+                                     (points[0].Y + points[1].Y) /2);
+        Point middleRight = new Point((points[0].X + points[2].X) / 2,
+                                      (points[0].Y + points[2].Y) /2);
+        Point middleBottom = new Point((points[1].X + points[2].X) / 2,
+                                       (points[1].Y + points[2].Y) /2);
+
+        SierpinskiFunc(new Point[]{ points[0], middleLeft, middleRight }, canvas, depth);
+        SierpinskiFunc(new Point[]{ middleLeft, points[1], middleBottom }, canvas, depth);
+        SierpinskiFunc(new Point[]{ middleRight, middleBottom, points[2] }, canvas, depth);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Paint paint = new Paint();
-        paint.setColor(Color.rgb(red, green, blue));
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(strokeWidth);
-        x = x + vx;
-        y = y + vy;
-        if (x > canvas.getWidth() - widthRect) {
-            vx = vx * -1;
-            modifyRect();
-        }
-        if (x < 0) {
-            vx = vx * -1;
-            modifyRect();
-        }
-        if (y > canvas.getHeight() - heightRect) {
-            vy = vy * -1;
-            modifyRect();
-        }
-        if (y < 0) {
-            vy = vy * -1;
-            modifyRect();
+
+        float mainX = canvas.getWidth() / 2;
+        float mainY = (float) (mainX * Math.sqrt(3)) + 100;
+        Point[] points = new Point[]{
+                new Point(mainX, 100),
+                new Point(0, mainY),
+                new Point(mainX * 2, mainY),
+        };
+        SierpinskiFunc(points, canvas, 0);
+        iteration++;
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        canvas.drawRect(x,y,widthRect + x,heightRect+y,paint);
-        int xSecond = x + 600;
-        canvas.drawRect(xSecond,y,widthRect +
-                xSecond,heightRect+y,paint);
-
-        canvas.drawCircle(xSecond + widthRect/2 +
-
-                madX,y+heightRect/2 + madY,10,paint);
-
-        canvas.drawCircle(x + widthRect/2 + madX,y+heightRect/2 +
-
-                madY,10,paint);
-
+        if(iteration >= 10) return;
         invalidate();
     }
 }
