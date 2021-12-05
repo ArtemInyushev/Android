3a170b6f9665aefa29293e62cac9d03daab9ee24
ArtemInyushev <art10inyushev@gmail.com>
2021-12-05 20:38:11 +0200

    lab5 task2 complete

--- labs/lab5/app/src/main/java/com/example/lab5/MyView.java
+++ labs/lab5/app/src/main/java/com/example/lab5/MyView.java
@@ -5,72 +5,75 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
+
+import java.util.Random;
+
 public class MyView extends View {
-    int x = 0;
-    int y = 0;
-    int widthRect = 300;
-    int heightRect = 200;
-    int strokeWidth = 20;
-    int vx = 10;
-    int vy = 10;
-    int blue = 0;
-    int green = 0;
-    int red = 0;
-    int madX = 0;
-    int madY = 0;
+    private float circleRadius = 400;
+    private float step = -400;
+    private float vstep = 1;
+    private float[] speeds = new float[]{1, 2, 4, 5, 8, 10, 20};
+    private boolean leftSide = true;
 
     public MyView(Context context) {
         super(context);
     }
 
-    void modifyRect() {
-        strokeWidth = (int)(Math.random()*100);
-        blue = (int)(Math.random()*255);
-        green = (int)(Math.random()*255);
-        red = (int)(Math.random()*255);
-        madX = (int)(Math.random()*100 - 50);
-        madY = (int)(Math.random()*100 - 50);
+    private float calculateX2Coordinate(float x1, float y1, float y2, boolean left){
+        // (x1 - x2)^2 + (y1 - y2)^2 = length^2
+        double tmp = Math.pow(circleRadius, 2) - Math.pow((y1 - y2), 2);
+        double x2 = 0;
+        if(left){
+            x2 = x1 - Math.sqrt(tmp);
+        }
+        else{
+            x2 = x1 + Math.sqrt(tmp);
+        }
+        return (float) x2;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        float circleMiddleX = canvas.getWidth() / 2;
+        float circleMiddleY = canvas.getHeight() / 2;
+
+        Paint paintCircle = new Paint();
+        paintCircle.setColor(Color.YELLOW);
+        paintCircle.setStyle(Paint.Style.STROKE);
+        paintCircle.setStrokeWidth(20);
+        canvas.drawCircle(circleMiddleX, circleMiddleY, circleRadius, paintCircle);
+
+
+        float squareMiddleY = circleMiddleY + step;
+        float squareMiddleX = calculateX2Coordinate(circleMiddleX, circleMiddleY, squareMiddleY, leftSide);
+        if(Math.abs(squareMiddleY - circleMiddleY) == circleRadius){
+            leftSide = !leftSide;
+            int rnd = new Random().nextInt(speeds.length);
+            vstep = speeds[rnd];
+        }
+        if(leftSide){
+            step -= vstep;
+        }
+        else {
+            step += vstep;
+        }
+
+        Paint paintMiddleSquarePoint = new Paint();
+        paintMiddleSquarePoint.setColor(Color.RED);
+        paintMiddleSquarePoint.setStyle(Paint.Style.STROKE);
+        paintMiddleSquarePoint.setStrokeWidth(20);
+        canvas.drawPoint(squareMiddleX, squareMiddleY, paintMiddleSquarePoint);
+
+
         Paint paint = new Paint();
-        paint.setColor(Color.rgb(red, green, blue));
+        paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(strokeWidth);
-        x = x + vx;
-        y = y + vy;
-        if (x > canvas.getWidth() - widthRect) {
-            vx = vx * -1;
-            modifyRect();
-        }
-        if (x < 0) {
-            vx = vx * -1;
-            modifyRect();
-        }
-        if (y > canvas.getHeight() - heightRect) {
-            vy = vy * -1;
-            modifyRect();
-        }
-        if (y < 0) {
-            vy = vy * -1;
-            modifyRect();
-        }
+        paint.setStrokeWidth(20);
 
-        canvas.drawRect(x,y,widthRect + x,heightRect+y,paint);
-        int xSecond = x + 600;
-        canvas.drawRect(xSecond,y,widthRect +
-                xSecond,heightRect+y,paint);
-
-        canvas.drawCircle(xSecond + widthRect/2 +
-
-                madX,y+heightRect/2 + madY,10,paint);
-
-        canvas.drawCircle(x + widthRect/2 + madX,y+heightRect/2 +
-
-                madY,10,paint);
-
+        canvas.drawRect(squareMiddleX - 150,squareMiddleY - 150,
+                       squareMiddleX + 150,squareMiddleY + 150, paint);
         invalidate();
     }
 }
