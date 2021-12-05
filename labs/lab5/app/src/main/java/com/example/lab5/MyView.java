961c34516e774a49434083bffdbeae27730aee3b
ArtemInyushev <art10inyushev@gmail.com>
2021-12-05 12:50:38 +0200

    lab5 task1.5 complete

--- labs/lab5/app/src/main/java/com/example/lab5/MyView.java
+++ labs/lab5/app/src/main/java/com/example/lab5/MyView.java
@@ -7,8 +7,12 @@ import android.graphics.Paint;
 import android.view.View;
 public class MyView extends View {
     int x = 0; // Координата по горизонталі лівого верхнього кута прямокутника
+    int y = 0; // Координата по вертикалі лівого верхнього кута прямокутника
     int widthRect = 300; // Ширина прямокутника
-    int vx = 10; // Скорость по оси Ox
+    int heightRect = 200; // Висота прямокутника
+    int strokeWidth = 20; // Товщина лінії прямокутника
+    int vx = 10; // Швидкість прямокутника по горизонталі
+    int vy = 10; // Швидкість прямокутника по вертикалі
 
     public MyView(Context context) {
         super(context);
@@ -17,14 +21,30 @@ public class MyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         Paint paint = new Paint();
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(20);
+        paint.setStrokeWidth(strokeWidth);
         x = x + vx;
-        if (x > canvas.getWidth() - widthRect) vx = vx * -1;
-        if (x < 0) vx = vx * -1;
-        canvas.drawRect(x,200,widthRect + x,500,paint);
+        y = y + vy;
+        if (x > canvas.getWidth() - widthRect) {
+            vx = vx * -1;
+            strokeWidth+=30;
+        }
+        if (x < 0) {
+            vx = vx * -1;
+            strokeWidth+=30;
+        }
+        if (y > canvas.getHeight() - heightRect) {
+            vy = vy * -1;
+            strokeWidth+=30;
+        }
+        if (y < 0) {
+            vy = vy * -1;
+            strokeWidth+=30;
+        }
+        canvas.drawRect(x,y,widthRect + x,heightRect+y,paint);
         invalidate();
     }
 }
