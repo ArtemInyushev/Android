e3dffeef1774e4e51c110055c700bc7902723a9a
ArtemInyushev <art10inyushev@gmail.com>
2021-12-05 12:54:03 +0200

    lab5 task1.6 complete

--- labs/lab5/app/src/main/java/com/example/lab5/MyView.java
+++ labs/lab5/app/src/main/java/com/example/lab5/MyView.java
@@ -6,16 +6,26 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 public class MyView extends View {
-    int x = 0; // Координата по горизонталі лівого верхнього кута прямокутника
-    int y = 0; // Координата по вертикалі лівого верхнього кута прямокутника
-    int widthRect = 300; // Ширина прямокутника
-    int heightRect = 200; // Висота прямокутника
-    int strokeWidth = 20; // Товщина лінії прямокутника
-    int vx = 10; // Швидкість прямокутника по горизонталі
-    int vy = 10; // Швидкість прямокутника по вертикалі
+    int x = 0;
+    int y = 0;
+    int widthRect = 300;
+    int heightRect = 200;
+    int strokeWidth = 20;
+    int vx = 10;
+    int vy = 10;
+    int blue = 0;
+    int green = 0;
+    int red = 0;
 
     public MyView(Context context) {
         super(context);
+    }
+
+    void modifyRect() {
+        strokeWidth = (int)(Math.random()*100);
+        blue = (int)(Math.random()*255);
+        green = (int)(Math.random()*255);
+        red = (int)(Math.random()*255);
     }
 
     @Override
@@ -23,28 +33,31 @@ public class MyView extends View {
         super.onDraw(canvas);
 
         Paint paint = new Paint();
-        paint.setColor(Color.RED);
+        paint.setColor(Color.rgb(red, green, blue));
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(strokeWidth);
+
         x = x + vx;
         y = y + vy;
         if (x > canvas.getWidth() - widthRect) {
             vx = vx * -1;
-            strokeWidth+=30;
+            modifyRect();
         }
         if (x < 0) {
             vx = vx * -1;
-            strokeWidth+=30;
+            modifyRect();
         }
         if (y > canvas.getHeight() - heightRect) {
             vy = vy * -1;
-            strokeWidth+=30;
+            modifyRect();
         }
         if (y < 0) {
             vy = vy * -1;
-            strokeWidth+=30;
+            modifyRect();
         }
+
         canvas.drawRect(x,y,widthRect + x,heightRect+y,paint);
+        
         invalidate();
     }
 }
